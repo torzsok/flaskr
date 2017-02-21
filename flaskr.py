fb07c7a5bbae65eca6d2b40fa28278f8e475cf3c
Törzsök Árpád <torzsokarpad@gmail.com>
2017-02-21 19:39:17 +0100

    uses peewee, an OMR instead of plain sql strings - method error

--- flaskr.py
+++ flaskr.py
@@ -10,184 +10,121 @@
     :license: BSD, see LICENSE for more details.
 """
 
-from sqlite3 import dbapi2 as sqlite3
-
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, _app_ctx_stack
+from flask import Flask, request, session, g, redirect, url_for, render_template, flash
 from peewee import *
 from werkzeug import check_password_hash, generate_password_hash
 
-db = SqliteDatabase('flaskr.db')
+db = SqliteDatabase('averynewflaskr.db')
 
 class BaseModel(Model):
     class Meta:
         database = db
 
-class Blogpost(BaseModel):
-    id = PrimaryKeyField()
-    title = CharField()
-    published = DateField()
-    text = CharField()
-    category = CharField()
-    author = ForeignKeyField(Person)
-
-
-class Person(BaseModel):
+class Author(BaseModel):
     user_id = PrimaryKeyField()
-    username = CharField()
+    username = CharField(unique=True)
     email = CharField()
     pw_hash = CharField()
 
+class Entry(BaseModel):
+    id = PrimaryKeyField()
+    title = CharField()
+    published = TimestampField()
+    text = CharField()
+    category = CharField()
+    author = ForeignKeyField(Author)
 
+db.connect()
 
+db.create_tables([Author, Entry], safe = True)
 
 
 
 # configuration
-DATABASE = '/tmp/flaskr.db'
+DATABASE = 'verynewflaskr.db'
 PER_PAGE = 30
 DEBUG = True
 SECRET_KEY = 'development key'
 
-# create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'sqlite_db'):
-        top.sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        top.sqlite_db.row_factory = sqlite3.Row
-    return top.sqlite_db
-
-
-def init_db():
-    """Initializes the database."""
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Creates the database tables."""
-    init_db()
-    print('Initialized the database.')
-
-
-@app.teardown_appcontext
-def close_database(exception):
-    """Closes the database again at the end of the request."""
-    top = _app_ctx_stack.top
-    if hasattr(top, 'sqlite_db'):
-        top.sqlite_db.close()
-
-
-def query_db(query, args=(), one=False):
-    """Queries the database and returns a list of dictionaries."""
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    return (rv[0] if rv else None) if one else rv
-
-
-def get_user_id(username):
-    """Convenience method to look up the id for a username."""
-    rv = query_db('select user_id from user where username = ?',
-                  [username], one=True)
-    return rv[0] if rv else None
-
-
-def get_username(user_id):
-    """Convenience method to look up the username for an id."""
-    rv = query_db('select username from user where user_id = ?',
-                  [user_id], one=True)
-    return rv[0] if rv else None
+def auth_user(user):
+    session['logged_in'] = True
+    session['user_id'] = user.id
+    session['username'] = user.username
+    flash('You are logged in as %s' % (user.username))
 
 @app.before_request
 def before_request():
-    g.user = None
-    if 'user_id' in session:
-        g.user = query_db('select * from user where user_id = ?',
-                          [session['user_id']], one=True)
+    g.db = db
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
 
 
 @app.route('/')
 def show_entries():
-    entries = query_db('select title, author, text from entries order by id desc')
+    entries = Entry.select().join(Author)
     return render_template('show_entries.html', entries=entries)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """Logs the user in."""
-    if g.user:
-        return redirect(url_for('show_entries'))
-    error = None
-    if request.method == 'POST':
-        user = query_db('''select * from user where
-            username = ?''', [request.form['username']], one=True)
-        if user is None:
-            error = 'Invalid username'
-        elif not check_password_hash(user['pw_hash'],
-                                     request.form['password']):
-            error = 'Invalid password'
+    if request.method == 'POST' and request.form['username']:
+        try:
+            user = Author.get(
+                username=request.form['username'])
+        except Author.DoesNotExist:
+            flash('The username entered is incorrect')
+
+        if not check_password_hash(user.pw_hash, request.form['password']):
+            flash('The password entered is incorrect')
         else:
-            flash('You were logged in')
-            session['user_id'] = user['user_id']
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+            auth_user(user)
+            return redirect(url_for('add_entry'))
+    return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """Registers the user."""
-    if g.user:
-        return redirect(url_for('show_entries'))
-    error = None
-    if request.method == 'POST':
-        if not request.form['username']:
-            error = 'You have to enter a username'
-        elif not request.form['email'] or \
-                '@' not in request.form['email']:
-            error = 'You have to enter a valid email address'
-        elif not request.form['password']:
-            error = 'You have to enter a password'
-        elif request.form['password'] != request.form['password2']:
-            error = 'The two passwords do not match'
-        elif get_user_id(request.form['username']) is not None:
-            error = 'The username is already taken'
-        else:
-            db = get_db()
-            db.execute('''insert into user (
-              username, email, pw_hash) values (?, ?, ?)''',
-              [request.form['username'], request.form['email'],
-               generate_password_hash(request.form['password'])])
-            db.commit()
-            flash('You were successfully registered and can login now')
-            return redirect(url_for('login'))
-    return render_template('register.html', error=error)
+    if request.method == 'POST' and request.form['username']:
+        try:
+            with db.transaction():
+                user = Author.create(
+                    username = request.form['username'],
+                    email = request.form['email'],
+                    pw_hash = generate_password_hash(request.form['password']))
+            auth_user(user)
+            return redirect(url_for('add_entry'))
+
+        except IntegrityError:
+            flash('That username is already taken')
+
+    return render_template('register.html')
 
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    if 'user_id' not in session:
-        abort(401)
-    db = get_db()
-    db.execute('insert into entries (title, text, author, category) values (?, ?, ?, ?)',
-               [request.form['title'], request.form['text'], get_username(session['user_id']), request.form['category']])
-    db.commit()
+    if not session['logged_in']:
+        return redirect(url_for('show_entries'))
+
+    entry = Entry.create(title = request.form['title'],
+                         text = request.form['text'],
+                         category = request.form['category'],
+                         author = Author.get(username=session['username']))
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
 
 @app.route('/logout')
 def logout():
-    session.pop('user_id', None)
+    session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
 
